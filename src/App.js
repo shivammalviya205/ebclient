662afb118670f3ed26510295d996c162901d4388
shivammalviya205 <86865439+shivammalviya205@users.noreply.github.com>
2023-03-11 00:12:04 +0530

    integrate payment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AdminEdit from "./admin/pages/AdminEdit/AdminEdit";
 import AdminHome from "./admin/pages/AdminHome/AdminHome";
 import AdminSignIn from "./admin/pages/authentication/AdminSignIn";
 import Slots from "./admin/pages/slots/Slots";
+import Payment from "./user/components/Payment";
+import PaymentSuccess from "./user/components/PaymentSuccess";
 import Home from "./user/Home/Home";
 import Profile from "./user/profile/Profile";
 import SignIn from "./user/signin/SignIn";
@@ -23,7 +25,9 @@ function App() {
     <Route path="/slots" element={<Slots/>} />
     <Route path="/home" element={<Home/>} />
     <Route path="/add" element={<AddExpert/>} />
-    
+    <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+    <Route path="/payment" element={<Payment/>} />
+
     <Route path="/expert/:expertId" element={<Profile/>} />
     <Route path="/editexpert/:id" element={<AdminEdit/>} />
     </Routes>
